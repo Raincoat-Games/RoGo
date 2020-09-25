cd18e4678e2527c335bbe8e387216524ba45eb1d
Doge <milesmurgaw@gmail.com>
2020-09-25 01:35:56 -0400

    First Endpoint!!!

--- group/group.go
+++ group/group.go
@@ -1,7 +1,19 @@
 //Package group provides functions to interact with Roblox groups.
 package group
 
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+	"time"
+)
+
 const endpoint = "https://groups.roblox.com/v1/groups/"
+
+var (
+	ErrGroupDoesntExist = errors.New("group doesn't exist")
+)
 
 //The Group struct provides information about a Roblox group.
 type Group struct {
@@ -25,5 +37,34 @@ type Owner struct {
 
 //The Get function retrieves info about a Roblox group.
 func Get(groupId int) (*Group, error) {
-	return nil, nil
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	groupIdString := strconv.Itoa(groupId)
+
+	//Create req
+	req, err := http.NewRequest("GET", endpoint+groupIdString, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	//Send Request
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = res.Body.Close() }()
+
+	//Check if exists
+	if res.StatusCode == http.StatusBadRequest {
+		return nil, ErrGroupDoesntExist
+	}
+
+	//Parse Response Body
+	var group *Group
+	err = json.NewDecoder(res.Body).Decode(&group)
+	if err != nil {
+		return nil, err
+	}
+
+	return group, nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/Clan-Labs/RoGo/group"
 
+func main() {
+	_, _ = group.Get(10)
 }
