0f9bbb437248215060870e10caf16bb53a50522f
ninjasgithub <60543166+ninjasgithub@users.noreply.github.com>
2020-10-03 14:24:29 +0100

    experimental cursor

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"fmt"
 	"github.com/Clan-Labs/RoGo/account"
 	"github.com/Clan-Labs/RoGo/group"
 	"log"
@@ -18,16 +19,12 @@ func TestMain(m *testing.M) {
 	acc := account.New(os.Getenv("COOKIE"))
 	g, err := group.Get(4953490, acc)
 	if err != nil { log.Fatal(err.Error()) }
-	err = g.Exile(1505886708)
-	if err != nil { log.Fatal(err.Error()) }
-	//r, err := g.GetJoinRequests()
+	//err = g.Exile(1505886708)
 	//if err != nil { log.Fatal(err.Error()) }
-	//for _, i := range r {
-	//	if i.Requester.Username == "killcripples" {
-	//		err = i.Accept()
-	//		if err != nil { log.Fatal(err.Error()) }
-	//		fmt.Println("Success")
-	//	}
-	//}
+	r, err := g.GetJoinRequests(1)
+	if err != nil { log.Fatal(err.Error()) }
+	for _, i := range r {
+		fmt.Printf("Username: %s\nUserID: %d\nCreated: %v\n\n", i.Requester.Username, i.Requester.UserID, i.Created)
+	}
 
 }
